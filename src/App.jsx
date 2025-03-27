63ec94b94c521a5ca537be1c96d5570a68d25033
Lucascortizo <lucascortizo768@gmail.com>
2025-03-26 23:50:51 -0300

    metade do site pronto

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/tasks";
+import './index.css';
+
+function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: "Estudar programação",
+      description: "Estudar programação para se tornar um desenvolvedor full-stack",
+      isCompleted: false,
+    },
+
+    {
+      id: 2,
+      title: "Estudar inglês",
+      description: "Estudar inglês para se tornar fluente",
+      isCompleted: false,
+    },
+
+    {
+      id: 3,
+      title: "Estudar matemática",
+      description: "Estudar matemática para se tornar um desenvolvedor full-stack",
+      isCompleted: false,
+    },
+    
+  ]);
+  return(
+    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px]">
+      <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciador de Tarefas</h1>  
+      <Tasks tasks={tasks}/>
+      <AddTask />
+      </div>
+    </div>
+  );  
+}
+
+export default App;
